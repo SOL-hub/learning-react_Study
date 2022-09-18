4bfe95d3b4d7462af02fc652ad3ca7c114dcf2ee
parksol <thfdl0317@naver.com>
2022-09-19 06:53:23 +0900

    feat : propsTypes사용2 -isRequired로  필수 propsTypes설정

--- learning-react/src/MyComponent.js
+++ learning-react/src/MyComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import propsTypes from 'prop-types';
-const MyComponent = ({name, children}) =>{
+const MyComponent = ({name, favoriteNumber, children}) =>{
 
     return (
         <>
             <p>나의 이름은{name}이오</p>
             <p>children 값은 {children}, 이겁니다.</p>
+            <br/>
+            <p>아이 라이크 페이브뤄릿 넘버 이즈 {favoriteNumber}</p>
         </>
     )
 };
@@ -16,7 +18,8 @@ MyComponent.defaultProps = {
 };
 
 MyComponent.propsTypes = {
-    name : propsTypes.string //값은 무조건 문자열형태로 전달해야한다고 설정
+    name : propsTypes.string, //값은 무조건 문자열형태로 전달해야한다고 설정
+    favoriteNumber: propsTypes.number.isRequired
 };
 
 export default MyComponent;
