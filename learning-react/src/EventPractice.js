de50efa8623f6e02e46973da93b6e5b8ddf65928
parksol <thfdl0317@naver.com>
2022-09-19 15:04:21 +0900

    feat : 함수형 컴포넌트사용하기

--- learning-react/src/EventPractice.js
+++ learning-react/src/EventPractice.js
@@ -1,14 +1,33 @@
-import React, {Component, useState} from 'react';
+import React, {useState} from 'react';
 
-class EventPractice extends Component {
-    render() {
+const EventPractice = () => {
+
+    const [username, setUsername] = useState('');
+    const [message, setMessage] = useState('');
+    const onChangeUsername =  e => setUsername(e.target.value);
+    const onChangeMessage = e => setMessage(e.target.value)
+
+    const onClick = ()=>{
+        alert(username + ':' + message);
+        setUsername('');
+        setMessage('');
+    };
+
+    const onKeyPress = e => {
+        if(e.key === 'Enter'){
+            onClick();
+        }
+    };
+
         return (
             <div>
                 <p>이벤트 연습</p>
-                <input name="message" placeholder='아무거나 입력하라'  onChange={(e)=>{console.log(e)}} />
+                <input name="username" placeholder='아무거나 입력하라' value={username} onChange={onChangeUsername} />
+                <input name="username" placeholder='아무거나 입력하라' value={message} onChange={onChangeMessage} onKeyPress={onKeyPress} />
+
+                <button onClick={onClick}>확인</button>
             </div>
         )
     }
-}
 
 export default EventPractice;
