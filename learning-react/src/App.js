3bcef8d7da0c2933c3a1ccbb50b05bc636850570
parksol <thfdl0317@naver.com>
2022-09-22 13:45:52 +0900

    feat : ref:DOM 에 이름달기, ref 사용

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import MyComponent from './MyComponent';
 import Say from './Say';
-import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 function App() {
 
@@ -11,9 +11,9 @@ function App() {
   return (
     <div className="App">
       <p>똘이의 리액트</p>
-      <MyComponent name={name} favoriteNumber={2}>사이에 껴있는</MyComponent>
+      <MyComponent name={name} favoriteNumber={2} ref={(ref) => this.myComponent=ref}>사이에 껴있는</MyComponent>
       <Say />
-      <EventPractice></EventPractice>
+      <ValidationSample/>
     </div>
   );
 }
