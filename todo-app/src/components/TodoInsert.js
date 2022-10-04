c5399dcc98a12fd6705d987ed7cb6b0580260b61
parksol <thfdl0317@naver.com>
2022-10-05 00:37:54 +0900

    feat : 인풋에 입력되는 값 관리, 오잉이것도 추가기능꺼임 +  코드 정리

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,36 +1,33 @@
 import { MdAddCircleOutline } from 'react-icons/md';
-import React , {useState, useCallback} from 'react';
+import React, { useState, useCallback } from 'react';
 import './TodoInsert.css';
 
-const TodoInsert = ({onInsert}) => {
-
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
-  const onChange = useCallback( e =>{
+  const onChange = useCallback((e) => {
     setValue(e.target.value);
-  },
-    []
-  );
+  }, []);
 
   const onSubmit = useCallback(
-    e=>{
+    (e) => {
       onInsert(value);
-      setValue('') //값 초기화
+      setValue(''); //값 초기화
 
       //submit 이벤트는 브라우저에서 새로고침을 발생시키기 때문에
       //방지하기 위해 이 함수를 호출한다
       e.preventDefault();
-      
     },
-    [onInsert, value]
+    [onInsert, value],
   );
 
   return (
     <form className="TodoInsert" onSubmit={onSubmit}>
-      <input placeholder="할 일을 입력하세요" 
-      value={value}
-      onChange={onChange}
-/>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAddCircleOutline />
       </button>
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,22 +1,25 @@
-import {MdCheckBoxOutlineBlank,MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
 import './TodoListItem.css';
 import cn from 'classnames';
 
-const TodoListItem = ({todo}) =>{
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
 
-    const {text, checked} = todo;
-
-    return (
-        <div className='TodoListItem'>
-        <div className={cn('checkbox',{checked})}>
-            {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-            <div className='text'>{text}</div>
-        </div>
-            <div className="remove">
-                <MdRemoveCircleOutline />
-            </div>
-        </div>
-    );
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
 };
 
 export default TodoListItem;
