a1da073532b9560949dbbe6a9235f0c5c0e965e4
parksol <thfdl0317@naver.com>
2022-10-05 12:48:17 +0900

    useState의 함수형 업데이트와 immer함께 쓰기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -11,13 +11,14 @@ const App = () => {
   });
 
   //input 수정을 위한 함수
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm({ ...form, [name]: [value] });
-    },
-    [form],
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      }),
+    );
+  }, []);
 
   //form등록을 위한 함수
   const onSubmit = useCallback(
@@ -31,7 +32,7 @@ const App = () => {
 
       //array에 새 항목 등록
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         }),
       );
@@ -47,19 +48,16 @@ const App = () => {
   );
 
   //항목을 삭제하는 함수
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1,
-          );
-        }),
-      );
-    },
-    [data],
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1,
+        );
+      }),
+    );
+  }, []);
 
   return (
     <div>
