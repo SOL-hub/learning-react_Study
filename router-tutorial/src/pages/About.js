2044c0a99336a86f4b4e373622f1f309b9633100
parksol <thfdl0317@naver.com>
2022-10-06 09:37:07 +0900

    feat : 쿼리스트링 사용

--- router-tutorial/src/pages/About.js
+++
@@ -1,9 +0,0 @@
-const About = () =>{
-    return 
-    <>
-    <h1>about</h1>
-    <p>어바웃 페이지</p>
-    </>
-}
-
-export default About;
--- router-tutorial/src/pages/Home.js
+++ router-tutorial/src/pages/Home.js
@@ -1,13 +1,27 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Home = () =>{
-    return (
-        <div>
-            <h1>홈</h1>
-            <p>가장 먼저보여지는 요것은 홈페이지</p>
-            <Link to="/about">About페이지</Link>
-        </div>
-    )
+const Home = () => {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>가장 먼저보여지는 요것은 홈페이지</p>
+      <ul>
+        <li>
+          <Link to="/about">About페이지</Link>
+        </li>
+        <li>
+          <Link to="/profiles/sol1">sol1의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/sol2">sol2의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/void">존재하지 않는 페이지입니다. </Link>
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
