2c3dff7753000dd311c6ea16492219385f37a0c5
parksol <thfdl0317@naver.com>
2022-10-06 10:19:14 +0900

    feat : useNavigate함수, replace 옵션 사용,

--- router-tutorial/src/Layout.js
+++ router-tutorial/src/Layout.js
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const gobBack = () => {
+    //이전 페이지로 이동
+    navigate(-1);
+  };
+
+  const goArticles = () => {
+    //articles 경로로 이동
+    navigate("/articles", { replace: true });
+  };
+
   return (
     <>
       <header style={{ background: "lightgray", padding: 16, fontSize: 25 }}>
-        header
+        <button onClick={gobBack}>뒤로가자</button>
+        <button onClick={goArticles}>게시글 목록</button>
       </header>
       <main>
         <Outlet></Outlet>
@@ -13,4 +26,4 @@ const Layout = () => {
   );
 };
 
-export default Outlet;
+export default Layout;
