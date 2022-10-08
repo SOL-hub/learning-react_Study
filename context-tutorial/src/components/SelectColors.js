e146364dcdf3bd554d486feb1f229ce2d07a824f
parksol <thfdl0317@naver.com>
2022-10-09 00:13:51 +0900

    feat : 색상 선택 컴포넌트 만들기2 - 버튼 클릭 시 바뀌도록

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,23 +1,33 @@
+import { ColorConsumer } from "../contexts/color";
+
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const SelectColor = () => {
   return (
     <div>
       <p>색상을 선택하라</p>
-
-      <div style={{ display: "flex" }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: "24px",
-              height: "24px",
-              cursor: "pointer",
-            }}
-          />
-        ))}
-      </div>
+      <ColorConsumer>
+        {({ actions }) => (
+          <div style={{ display: "flex" }}>
+            {colors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: "24px",
+                  height: "24px",
+                  cursor: "pointer",
+                }}
+                onClick={() => actions.setColor(color)}
+                onContextMenu={(e) => {
+                  e.preventDefault(); //마우스 오른쪽 버튼 클릭시 메뉴 뜨는 거 무시하기
+                  actions.setSubcolor(color);
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </ColorConsumer>
       <hr />
     </div>
   );
