700abdc55621cb4e6e8a34cfdadd66ec767f3650
parksol <thfdl0317@naver.com>
2022-10-09 19:28:07 +0900

    feat : 버튼 누르는대로 +1, -1되게하기~

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,31 +1,24 @@
 import Counter from "../components/Counter";
-import {connect} from 'react-redux'; 
+import { connect } from "react-redux";
+import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({number, increase, decrease}) => {
+const CounterContainer = ({ number, increase, decrease }) => {
   return (
-  <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
   );
 };
 
-const mapStateToProps = state => ({
-    number:state.counter.number,
-
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
 });
 
-const mapDispatchToProps = dispatch => ({
-    //임시함수
-    increase : () => {
-        console.log('increase');
-    },
-
-    decrease : () => {
-        console.log('decrease');
-    },
-
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(CounterContainer);
-
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
