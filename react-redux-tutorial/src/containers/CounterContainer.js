6c8418786ea06f7bb52f5b284c816c762821d5b3
parksol <thfdl0317@naver.com>
2022-10-09 20:28:37 +0900

    feat: 더 편하게 하는 방법쓰(객체 형태로 넣어주기)

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,3 @@
-import { bindActionCreators } from "redux";
 import Counter from "../components/Counter";
 import { connect } from "react-redux";
 import { increase, decrease } from "../modules/counter";
@@ -13,5 +12,5 @@ export default connect(
   (state) => ({
     number: state.counter.number,
   }),
-  (dispatch) => bindActionCreators({ increase, decrease }, dispatch),
+  { increase, decrease },
 )(CounterContainer);
