3fecc440f303fbfba6c7abc1cd93298ce38a9948
parksol <thfdl0317@naver.com>
2022-10-09 22:29:06 +0900

    feat : Todos컴포넌트를위한TodosContainer 작성 후 교체, props사용

--- react-redux-tutorial/src/components/Todos.js
+++ react-redux-tutorial/src/components/Todos.js
@@ -1,9 +1,16 @@
 const TodoItem = ({ todo, onToggle, onRemove }) => {
   return (
     <>
-      <input type="checkbox" />
-      <span>예제테스트</span>
-      <button>삭제</button>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
     </>
   );
 };
@@ -18,22 +25,30 @@ const Todos = ({
 }) => {
   const onSubmit = (e) => {
     e.preventDefault();
+    onInsert(input);
+    onChangeInput(""); //등록 후 인풋 초기화
   };
+
+  const onChange = (e) => onChangeInput(e.target.value);
 
   return (
     <>
-        <form onSubmit={onSubmit}>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
         <button type="submit">등록</button>
-        </form>
-    <div>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-    </div>
+      </form>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
+      </div>
     </>
-  )
+  );
 };
 
 export default Todos;
