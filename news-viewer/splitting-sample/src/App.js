eb72cfc5ca27cd19df609393afd625e1a0d5bea4
parksol <thfdl0317@naver.com>
2022-10-11 10:58:41 +0900

    feat : 플젝 초기 생성, React.lazy와 Suspense사용하기

---
+++ news-viewer/splitting-sample/src/App.js
@@ -0,0 +1,26 @@
+import { useState, Suspense } from 'react';
+import './App.css';
+import React from 'react';
+
+const SplitMe = React.lazy(() => import('./SplitMe'));
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>하이</p>
+        <p onClick={onClick}>하이 리액트</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
+}
+
+export default App;
