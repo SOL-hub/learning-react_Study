d1f846a23b831b3a8f0415920c717346ce6f07cb
parksol <thfdl0317@naver.com>
2022-10-11 13:51:52 +0900

    feat : 정적파일 제공하기2

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -4,12 +4,43 @@ import express from "express";
 import { StaticRouter } from "react-router-dom";
 import App from "./App";
 import path from "path";
+import fs from "fs";
+
+//assert-manifest.json에서 파일 경로들을 조회
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/assert-manifest.json"), "utf-8")
+);
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no"
+      />
+      <meta name="theme-color" content="#000000" />
+      <title>React App</title>
+      <lik href="${manifest.files["main.css"]}" rel="stylesheet" />
+    </head>
+    <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root">
+        ${root}
+      </div>
+      <script src="${manifest.files["main.js"]}"></script>
+    </body>
+    </html>
+      `;
+}
 
 const app = express();
 
-// 서버사이드 렌더링을 처리 할 핸들러 함수
+// 서버사이드 렌더링을 처리 할 핸들러 함수입니다.
 const serverRender = async (req, res, next) => {
-  // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링.
+  // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
 
   const context = {};
 
@@ -19,8 +50,8 @@ const serverRender = async (req, res, next) => {
     </StaticRouter>
   );
 
-  const root = ReactDOMServer.renderToString(jsx); // 렌더링.
-  res.send(root); // 결과물을 응답.
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(createPage(root));
 };
 
 const serve = express.static(path.resolve("./build"), {
@@ -30,7 +61,7 @@ const serve = express.static(path.resolve("./build"), {
 app.use(serve);
 app.use(serverRender);
 
-// 5000 포트로 서버를 가동.
+// 5000 포트로 서버를 가동합니다.
 app.listen(5000, () => {
   console.log("Running on http://localhost:5000");
 });
