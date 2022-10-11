c1e3e58f620e6ef30e3c090e27dcd4b37975491b
parksol <thfdl0317@naver.com>
2022-10-12 01:00:30 +0900

    feat : 서버 띄워 확인

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,1 +1,11 @@
-const hello = "하이";
+const koa = require('koa');
+
+const app = new koa();
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
