b1849dedc67b26be75f7c5238986aef880e98a50
parksol <thfdl0317@naver.com>
2022-10-12 10:55:31 +0900

    feat:쿼리파라미터 포함여부에 따라 미들웨어 처리

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,8 +2,23 @@ const koa = require('koa');
 
 const app = new koa();
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
+app.use((ctx, next) => {
+    console.log(ctx.url);
+    console.log(1);
+    if(ctx.query.authorized !=='1') {
+        ctx.status = 401; //Unauthorized
+        return;
+    }
+    next();
+});
+
+app.use((ctx, next) =>{
+    console.log(2);
+    next();
+});
+
+app.use(ctx =>{
+    ctx.body = 'hello world';
 });
 
 app.listen(4000, () => {
